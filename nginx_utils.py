afbb9673a5255d79eb14d75a52b3e8a41f82b779
faris <faris@elham.ai>
2024-12-21 02:33:30 +0300

    str(model_id)

--- nginx_utils.py
+++ nginx_utils.py
@@ -25,6 +25,7 @@ def add_model_to_nginx_config(user_id,model_name, container_port):
     # Create the location block for the new model path
     location_block = f"""
     location  /{user_id}/{model_name} {{
+        rewrite ^/{user_id}/{model_name}(/.*)$ $1 break;
         proxy_pass http://{user_id}_{model_name}_upstream/{user_id}/{model_name};
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
@@ -88,6 +89,7 @@ def delete_model_from_nginx_config(user_id,model_name, container_port):
     # Create the location block for the new model path
     location_block = f"""
     location /{user_id}/{model_name} {{
+        rewrite ^/{user_id}/{model_name}(/.*)$ $1 break;
         proxy_pass http://{user_id}_{model_name}_upstream/{user_id}/{model_name};
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
